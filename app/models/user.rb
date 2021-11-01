8ebb4f6006595b1efd194b67f4dd6402fd9fa48a
hanns.freed <vanalgant0924@gmail.com>
2021-11-01 07:16:07 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
+  
+  has_many :tasks
 end
